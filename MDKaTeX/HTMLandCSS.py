0fb5f2f7ae86ca621eae1bd03303402510bd7049
eljamm <fedi.jamoussi@protonmail.ch>
2024-03-27 17:18:55 +0100

    Reuse HTML template code for common note types
    The front and back for the base note type shares the exact code, except
    for the render() and show() function. Therefore, if makes more sense to
    extract their common dependency into a template of its own, which makes
    maintaining the code much easier.
    
    In the shared render() and show() functions, the fields are read
    dynamically as the card changes, toggling only the available fields.
    
    This is the same for the cloze note type.

--- MDKaTeX/HTMLandCSS.py
+++ MDKaTeX/HTMLandCSS.py
@@ -13,16 +13,19 @@ def path_relative_to_this_file(path):
     return os.path.join(os.path.dirname(__file__), path)
 
 
-HTMLforEditor = file_to_string(
-    path_relative_to_this_file("js/HTMLforEditor.js"))
+def read_file(path):
+    return file_to_string(path_relative_to_this_file(path))
 
-front = file_to_string(path_relative_to_this_file("html/front.html"))
 
-back = file_to_string(path_relative_to_this_file("html/back.html"))
+HTMLforEditor = read_file("js/HTMLforEditor.js")
 
-front_cloze = file_to_string(
-    path_relative_to_this_file("html/front_cloze.html"))
+script_base = read_file("html/script_base.html")
+script_cloze = read_file("html/script_cloze.html")
 
-back_cloze = file_to_string(path_relative_to_this_file("html/back_cloze.html"))
+front = read_file("html/front.html") + script_base
+back = read_file("html/back.html") + script_base
+
+front_cloze = read_file("html/front_cloze.html") + script_cloze
+back_cloze = read_file("html/back_cloze.html") + script_cloze
 
 css = read_file("css/style_import.css")
